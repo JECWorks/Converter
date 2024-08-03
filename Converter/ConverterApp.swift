a423b3375e5be01364a1a7af2568e69999cdadf6
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-08-02 21:38:44 -0400

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Jason Cox on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

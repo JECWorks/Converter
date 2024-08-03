756c08b55aa03f55c0a85976eb5258448c88ee73
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-08-03 19:19:49 -0400

    Initial commit for 100DoSUI Challenge 1

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var tempStart = 0.0
+    @State private var tempConvert = 0.0
+    @State private var unitStart = "Fahrenheit"
+    @State private var unitConvert = "Celsius"
+    let units = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    func convertTemperature() {
+            // Add your conversion logic here
+            // For example, converting from Celsius to Fahrenheit:
+        if unitStart == "Celsius" && unitConvert == "Fahrenheit" {
+            tempConvert = (tempStart * 9/5) + 32
+            }
+        if unitStart == "Fahrenheit" && unitConvert == "Celsius" {
+            tempConvert = (tempStart - 32) * 5/9
+            }
+        if unitStart == "Celsius" && unitConvert == "Kelvin" {
+            tempConvert = tempStart + 273.15
         }
-        .padding()
+        if unitStart == "Kelvin" && unitConvert == "Celsius" {
+            tempConvert = tempStart - 273.15
+        }
+        if unitStart == "Fahrenheit" && unitConvert == "Kelvin" {
+            tempConvert = ((tempStart - 32) * 5/9) + 273.15
+            }
+        if unitStart == "Kelvin" && unitConvert == "Fahrenheit" {
+            tempConvert = (tempStart * 9/5) - 459.67
+            }
+
+        }
+
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Starting temp") {
+                    TextField("Starting value", value: $tempStart, format: .number)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Unit", selection: $unitStart) {
+                        ForEach(units, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section ("Converted temp") {
+                    Text("\(tempConvert)")
+                    
+                    Picker("Unit", selection: $unitConvert) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Button("Convert") {
+                    convertTemperature()
+                }
+            }
+            .navigationTitle("Temperature Converter")
+            .padding()
+        }
     }
 }
 
